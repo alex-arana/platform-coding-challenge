dd473df184f0643a161351c9a049ecadb67d0627
Alex Arana <alex.arana@gmail.com>
2016-02-07 15:27:46 +1100

    Modified run script for embedded web server.

--- latency-measurement-endpoint/run.sh
+++ latency-measurement-endpoint/run.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 cd $(dirname $0)
 
-mvn clean package spring-boot:run
+# This is the port number that the embedded web server will run on
+PORT=8080
+
+# We detect the java executable to use according to the following algorithm:
+#
+# 1. If it is located in JAVA_HOME, then we use that; or
+# 2. Use the java that is in the command path.
+#
+if [ -d "$JAVA_HOME" -a -x "$JAVA_HOME/bin/java" ]; then
+    JAVACMD="$JAVA_HOME/bin/java"
+else
+    JAVACMD=java
+fi
+
+$JAVACMD -Xmx128m -jar target/dependency/jetty-runner.jar --port $PORT target/*.war
+
 exit
